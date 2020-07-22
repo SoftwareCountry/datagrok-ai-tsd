4ebaf4e24343d69ea25689079d18b92473076676
Alex Shevnin <alexander.shevnin@arcadia.spb.ru>
2020-07-22 23:03:45 +0300

    added index.d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="src/chem.d.ts"/>
+/// <reference path="src/const.d.ts"/>
+/// <reference path="src/dapi.d.ts"/>
+/// <reference path="src/data.d.ts"/>
+/// <reference path="src/dataframe.d.ts"/>
+/// <reference path="src/docking.d.ts"/>
+/// <reference path="src/entities.d.ts"/>
+/// <reference path="src/events.d.ts"/>
+/// <reference path="src/functions.d.ts"/>
+/// <reference path="src/grid.d.ts"/>
+/// <reference path="src/ml.d.ts"/>
+/// <reference path="src/shell.d.ts"/>
+/// <reference path="src/utils.d.ts"/>
+/// <reference path="src/view.d.ts"/>
+/// <reference path="src/viewer.d.ts"/>
+/// <reference path="src/widgets.d.ts"/>
+/// <reference path="src/wrappers.d.ts"/>
+/// <reference path="dg.d.ts"/>
+/// <reference path="grok.d.ts"/>
+/// <reference path="ui.d.ts"/>
+
+
